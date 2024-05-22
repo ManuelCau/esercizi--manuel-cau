bc8bd4f154d94bb1c4cb7749254390a3be31c598
Manuel Cau <caumanuel10@gmail.com>
2024-05-22 14:25:20 +0200

    commit

---
+++ eventi dom/esercizio eventi/esercizi dom/index.js
@@ -0,0 +1,10 @@
+function addNew() {
+  const firstUl = document.querySelector("#my-ul");
+  const firstButton = document.querySelector("#my-button");
+  const input = document.querySelector("#my-input");
+  
+
+  firstButton.addEventListener("click", (event) => {
+    li.createElement('li')
+  });
+}
