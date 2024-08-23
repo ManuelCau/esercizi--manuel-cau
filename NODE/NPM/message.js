d55760f98dac3344f15ad13de3b41edc55a340c5
Manuel Cau <caumanuel10@gmail.com>
2024-08-23 16:48:01 +0200

    m

---
+++ NODE/NPM/message.js
@@ -0,0 +1,15 @@
+const figlet = require("figlet");
+
+function outputMessage() {
+  console.log(
+    figlet.textSync("Boo!", {
+      font: "Ghost",
+      horizontalLayout: "default",
+      verticalLayout: "default",
+      width: 80,
+      whitespaceBreak: true,
+    })
+  );
+}
+
+outputMessage();
