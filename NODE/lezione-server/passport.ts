7fb62df977cd37664e9dd7fe9c4f2b1fa14d3235
Manuel Cau <caumanuel10@gmail.com>
2024-09-12 01:03:42 +0200

    m

--- NODE/lezione-server/passport.ts
+++ NODE/lezione-server/passport.ts
@@ -1,10 +1,17 @@
-import * as dotenv from "dotenv";
-dotenv.config();
-
+import "dotenv/config";
 import passport from "passport";
 import passportJWT from "passport-jwt";
+import { db } from "./db.js";
 
 const { SECRET } = process.env;
+
+if (!SECRET) {
+  throw new Error("JWT secret not defined in environment variables.");
+}
+
+interface JwtPayload {
+  id: string;
+}
 
 passport.use(
   new passportJWT.Strategy(
@@ -13,10 +20,13 @@ passport.use(
       jwtFromRequest: passportJWT.ExtractJwt.fromAuthHeaderAsBearerToken(),
     },
     async (payload, done) => {
-      const user = db.one(`SELECT * FROM users WHERE id=$1`, payload.id);
-      console.log(user);
       try {
-        return user ? done(null, user) : done(new Error("user not found"));
+        const user = await db.one(
+          `SELECT * FROM users WHERE id=$1`,
+          payload.id
+        );
+
+        return user ? done(null, user) : done(new Error("User not found."));
       } catch (error) {
         done(error);
       }
--- NODE/lezione-server/server copy.ts
+++ NODE/lezione-server/server copy.ts
@@ -1,51 +1,54 @@
+import "dotenv/config";
 import express from "express";
 import "express-async-errors";
 import morgan from "morgan";
-import {
-  getAll,
-  getOneById,
-  create,
-  updateById,
-  deleteById,
-} from "./controllers/planets.js";
+import planetRouter from "./routes/router.js";
+import { logIn, signUp, logOut } from "./controllers/users.js";
+import authorize from "./authorize.js";
 import multer from "multer";
-import { log } from "console";
-import { createImage } from "./controllers/planets copy.js";
+/* import router from "./routes/router.js";*/
+import { createImage } from "./controllers/planets.js";
+import "./passport.js"
 
-const app = express();
-const port = 3000;
+// Applicazione per caricare file nel database
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, file.originalname);
-  },
-  filename: (req, file, cb) => {
     cb(null, "./uploads");
   },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
 });
+
 const upload = multer({ storage });
+
+// Creazione nuova applicazione Express & definizione porta, ereditata da .env o default 3000
+const app = express();
+const port = process.env.PORT || 3000;
+
 app.use("/uploads", express.static("uploads"));
 app.use("/static", express.static("static"));
+
+// Dotare app di morgan per il logging richieste & express per fare il parse dei corpi richieste come JSON
 app.use(morgan("dev"));
 app.use(express.json());
 
-app.get("/api/planets", getAll);
+// Importare routes
+app.use("/api", planetRouter);
 
-app.get("/api/planets/:id", getOneById);
-
-//creare un nuovo pianeta
-
-app.post("/api/planets", create);
-
-//aggiornare un pianeta con PUT
-
-app.put("/api/planets/:id", updateById);
-
-//eliminare con DELETE
-app.delete("/api/planets/:id", deleteById);
-
+// Indicare indirizzo per l'upload di immagine
 app.post("/api/planets/:id/image", upload.single("image"), createImage);
-console.log(req.file);
 
+// Implementazione Login
+app.post("/api/users/login", logIn);
+
+// Implementazione Sign Up
+app.post("/api/users/signup", signUp);
+
+// Implementazione Logout
+app.get("/api/users/logout", authorize, logOut);
+
+// Avvio server e ascolto sulla porta dichiarata
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
 });
